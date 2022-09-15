1106d6e6a38b0f9105244b00dc313c6a05ebd205
Zobachov Vladimir <zobachov@gmail.com>
2022-09-15 15:12:06 +0500

    fix: joi validation email

--- validator.js
+++ validator.js
@@ -10,6 +10,8 @@ const validateLink = (link) => {
 
 const validateUserBody = celebrate({
   body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
     avatar: Joi.string().required().custom(validateLink),
