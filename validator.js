3ce2d686bf3d7a76a60e607113f9bf6391250b03
Zobachov Vladimir <zobachov@gmail.com>
2022-09-16 22:05:28 +0500

    fix: validation error

--- validator.js
+++ validator.js
@@ -1,5 +1,4 @@
 const { celebrate, Joi } = require('celebrate');
-const { ValidationError } = require('./errorsClasses/ValidationError');
 
 const validateLink = (link) => {
   const regex = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/g;
@@ -9,13 +8,12 @@ const validateLink = (link) => {
   throw new Error('Некорректная ссылка');
 };
 
-const validateEmail = (email) => {
-  const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
-  if (regex.test(email)) {
-    return email;
-  }
-  throw new ValidationError('Некорректный email');
-};
+const validateLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
 
 const validateUserBody = celebrate({
   body: Joi.object().keys({
@@ -30,5 +28,5 @@ const validateUserBody = celebrate({
 module.exports = {
   validateLink,
   validateUserBody,
-  validateEmail
+  validateLogin
 };
