9e2e018a325178d4f13cfaacac37d3709ef95139
Zobachov Vladimir <zobachov@gmail.com>
2022-09-16 17:42:28 +0500

    fix: errors auth 401

--- midlewares/auth.js
+++ midlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const {NotAuthError} = require("../errorsClasses/NotAuthError");
 
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -6,7 +7,7 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'SECRET');
   } catch (err) {
-    next(new Error('Вы не авторизованы!'));
+    next(new NotAuthError('Вы не авторизованы!'));
   }
   req.user = payload;
   next();
