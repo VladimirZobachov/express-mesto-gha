f482e781850fcc7eeec4836c39f9392ed498a3bf
Zobachov Vladimir <zobachov@gmail.com>
2022-09-18 13:33:07 +0500

    fix: 404 page

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,16 @@ const {
   getUsers, getUserById, updateUser, updateUserAvatar, getCurUser,
 } = require('../controllers/users');
 const { validateUserId, validateUpdateUser, validateUpdateAvatar } = require('../validator');
+const { NotFoundError } = require("../errorsClasses/NotFoundError");
 
 userRoutes.get('/users', getUsers);
 userRoutes.get('/users/me', getCurUser);
 userRoutes.get('/users/:userId', validateUserId, getUserById);
 userRoutes.patch('/users/me', validateUpdateUser, updateUser);
 userRoutes.patch('/users/me/avatar', validateUpdateAvatar, updateUserAvatar);
+
+userRoutes.use((req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
 
 module.exports = { userRoutes };
